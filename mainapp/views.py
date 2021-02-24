872d343a823800993b8a3adac00dc0e42238cdb3
Alexey <alexeyhenke@yandex.ru>
2021-02-25 00:48:40 +0300

    [lesson-2] fix static hardcode

--- mainapp/views.py
+++ mainapp/views.py
@@ -4,8 +4,14 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'mainapp/index.html')
+    context = {
+        'title': 'GeekShop',
+    }
+    return render(request, 'mainapp/index.html', context)
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    context = {
+        'title': 'GeekShop - Products',
+    }
+    return render(request, 'mainapp/products.html', context)
