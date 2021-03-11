a040a6ac79de001ad1011d6088c3130a62511f22
Alexey <alexeyhenke@yandex.ru>
2021-03-12 00:36:33 +0300

    [lesson-4] added register user and logout

--- authapp/views.py
+++ authapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.contrib import auth, messages
 from django.urls import reverse
-from authapp.forms import UserLoginForm, UserCreationForm
+from authapp.forms import UserLoginForm, UserRegisterForm
 
 
 def login(request):
@@ -23,4 +23,17 @@ def login(request):
 
 
 def register(request):
-    return render(request, 'authapp/register.html')
+    if request.method == 'POST':
+        form = UserRegisterForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('auth:login'))
+    else:
+        form = UserRegisterForm()
+    context = {'form': form}
+    return render(request, 'authapp/register.html', context)
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))
